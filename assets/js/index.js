4af7461cd115e95037627f92e991eb9960039765
paluku-ezechiel <palukuezechiel@gmail.com>
2026-08-05 11:32:58 +0200

    initialisation du projet

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// const button = document.querySelector(".icon-share");
+// const shareTooltip = document.querySelector(".share-tooltip");
+// if (button) {
+//   button.addEventListener("click", () => {
+//     shareTooltip.classList.toggle("hidden");
+//   });
+// }
