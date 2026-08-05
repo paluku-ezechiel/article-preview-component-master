d29156c08e13b33b6d70cf96cff0ad448076b219
paluku-ezechiel <palukuezechiel@gmail.com>
2026-08-05 15:02:51 +0200

    ajout code javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
-// const button = document.querySelector(".icon-share");
-// const shareTooltip = document.querySelector(".share-tooltip");
-// if (button) {
-//   button.addEventListener("click", () => {
-//     shareTooltip.classList.toggle("hidden");
-//   });
-// }
+const button = document.querySelector(".icon-share");
+const shareTooltip = document.querySelector(".share-tooltip");
+if (button) {
+  button.addEventListener("click", () => {
+    shareTooltip.classList.toggle("hidden");
+  });
+}
